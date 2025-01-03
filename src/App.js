ac3b776de9d1aad23d50f44690edec80bdada55a
Sunanth Samala <sunanth.samala7@gmail.com>
2025-01-03 09:26:03 +0530

    1

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Footer from './Screens/Footer';
 import Home from './Screens/Home';
