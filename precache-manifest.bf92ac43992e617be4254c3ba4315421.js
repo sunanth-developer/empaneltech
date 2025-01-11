f38ffac8767f9cc70d73b0c648a9b29b52343354
Sunanth Samala <sunanth.samala7@gmail.com>
2025-01-11 15:50:39 +0530

    Updates

---
+++ precache-manifest.bf92ac43992e617be4254c3ba4315421.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1b7e233afb6b7933a4a883da9962ad9",
+    "url": "/empaneltech/index.html"
+  },
+  {
+    "revision": "594ab3d4f3f54d8d41cc",
+    "url": "/empaneltech/static/css/main.3a7d135c.chunk.css"
+  },
+  {
+    "revision": "a7259f264243c4df7d75",
+    "url": "/empaneltech/static/js/2.308fe297.chunk.js"
+  },
+  {
+    "revision": "af9aaea1c42d94b854b71680195ea242",
+    "url": "/empaneltech/static/js/2.308fe297.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de1669870ac77c2299f9",
+    "url": "/empaneltech/static/js/3.98b04219.chunk.js"
+  },
+  {
+    "revision": "594ab3d4f3f54d8d41cc",
+    "url": "/empaneltech/static/js/main.25326b21.chunk.js"
+  },
+  {
+    "revision": "6b019894aef6fe9bf330",
+    "url": "/empaneltech/static/js/runtime-main.37b6a0d3.js"
+  },
+  {
+    "revision": "ce21fd288a5026e0fe1f9bf0a489615a",
+    "url": "/empaneltech/static/media/BusinessServices.ce21fd28.jpg"
+  },
+  {
+    "revision": "3e8ba4255bd0241685de593480d5b302",
+    "url": "/empaneltech/static/media/Custom-Application-Development.3e8ba425.jpg"
+  },
+  {
+    "revision": "1e681056ceb61c746ef1498f4fe2b6ed",
+    "url": "/empaneltech/static/media/Customer-Support.1e681056.jpg"
+  },
+  {
+    "revision": "4a6275eb2814224d28c6d42e17d4a327",
+    "url": "/empaneltech/static/media/Outsourcing1.4a6275eb.jpg"
+  },
+  {
+    "revision": "dc9701627583eb316554e2ed95ccc867",
+    "url": "/empaneltech/static/media/Staff-Augmentation.dc970162.jpg"
+  },
+  {
+    "revision": "95482b69c70d86d8505056e03f35439d",
+    "url": "/empaneltech/static/media/Utility-Services.95482b69.jpg"
+  },
+  {
+    "revision": "7ea5dc5e73e4ce1dc4a74f2d4c4ed9e8",
+    "url": "/empaneltech/static/media/app_exchange.7ea5dc5e.jpg"
+  },
+  {
+    "revision": "343cde3a97bf3bf508763a77ef0e4b35",
+    "url": "/empaneltech/static/media/logo.343cde3a.png"
+  },
+  {
+    "revision": "7a17b2a3e0d0791fffb7e03405d8ceaa",
+    "url": "/empaneltech/static/media/marketing.7a17b2a3.jpg"
+  },
+  {
+    "revision": "c9a0769ba48c2c284b76178e2837df60",
+    "url": "/empaneltech/static/media/platform-development.c9a0769b.jpg"
+  },
+  {
+    "revision": "95576a8cc01a2b1533b61f98e4f658a1",
+    "url": "/empaneltech/static/media/qa.95576a8c.jpg"
+  },
+  {
+    "revision": "5fd4f425a591ba6e6590e448ec8385b7",
+    "url": "/empaneltech/static/media/reba.5fd4f425.png"
+  },
+  {
+    "revision": "de7a6456cb6d812ac4cdf967ab0c8ec6",
+    "url": "/empaneltech/static/media/sales.de7a6456.jpg"
+  }
+]);
